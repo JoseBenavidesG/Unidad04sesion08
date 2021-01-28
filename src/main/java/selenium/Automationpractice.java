13c49b35e2136b9d859c3eeae2deb47bd75b4105
JoseBenavidesG <josebenavidesg@gmail.com>
2021-01-27 22:43:56 -0300

    ejercicio practico unidad IV sesion08

---
+++ src/main/java/selenium/Automationpractice.java
@@ -0,0 +1,13 @@
+package selenium;
+
+/**
+ * Hello world!
+ *
+ */
+public class Automationpractice 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
